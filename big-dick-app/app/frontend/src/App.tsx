a8b663f44037197cc804c06ed2d1a707a993307e
hosua <hosua.o@gmail.com>
2025-11-28 00:48:05 -0500

    Fix LSP settings and expose trafik UI

--- big-dick-app/app/frontend/src/App.tsx
+++ big-dick-app/app/frontend/src/App.tsx
@@ -1,15 +1,17 @@
 import "./App.css";
-
 import CoolTable from "./pages/CoolTable";
-
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 function App() {
+  const [result, setResult] = useState<null | string>(null);
   const fetchFromApi = async () => {
+    setResult(null);
     try {
       const response = await fetch("/api/test");
       const result = await response.json();
       console.log(result);
+      setResult(result.message);
     } catch (e) {
       console.error(e);
     }
@@ -19,8 +21,10 @@ function App() {
     <>
       <CoolTable />
       <Button variant="outline" onClick={fetchFromApi}>
-        button
+        API Test
       </Button>
+      <br />
+      {result}
     </>
   );
 }
