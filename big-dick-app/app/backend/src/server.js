876f86d4565d9c05b33c1b8d5f2b01cb8221663f
hosua <hosua.o@gmail.com>
2025-11-27 21:35:50 -0500

    First push

---
+++ big-dick-app/app/backend/src/server.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/api", (req, res) => {
+  console.log("You hit da API bitch");
+  res.send("You hit da API bitch");
+});
+
+console.log(`Running server on port ${port}...`);
+app.listen(port);
